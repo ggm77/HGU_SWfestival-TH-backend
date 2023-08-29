33e18ae0ffe8b987ed031511c7a3b5ebebc13273
ggm77 <shmshm86@naver.com>
2023-08-30 01:02:38 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_FILE = os.path.join(BASE_DIR, "secrets.json")
+secrets = json.loads(open(SECRET_FILE).read())
+
+app = FastAPI()
+
